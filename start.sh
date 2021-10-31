c942626ad5600d9221c321fd44890a1739c7aafb
gkbnypwngmfba35 <91242607+gkbnypwngmfba35@users.noreply.github.com>
2021-10-31 05:31:17 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # configs
-AUUID=7c334ee2-ad5c-4816-8674-57681221e562
+AUUID=43e5e0f7-164e-446f-8f55-e4bcd0bcc943
 CADDYIndexPage=https://github.com/AYJCSGM/mikutap/archive/master.zip
 CONFIGCADDY=https://raw.githubusercontent.com/Lbingyi/HerokuXray/master/etc/Caddyfile
 CONFIGXRAY=https://raw.githubusercontent.com/Lbingyi/HerokuXray/master/etc/xray.json
